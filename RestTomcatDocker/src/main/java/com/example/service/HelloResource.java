a08423496c95d47491fb777c3af93b62bbe35884
Oleksii Bondar <olexiy.bondar@gmail.com>
2025-03-22 08:49:41 +0100

    added logging

--- RestTomcatDocker/src/main/java/com/example/service/HelloResource.java
+++ RestTomcatDocker/src/main/java/com/example/service/HelloResource.java
@@ -1,6 +1,9 @@
 package com.example.service;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,10 +11,13 @@ import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class HelloResource {
+    
+     private static final Logger logger = LoggerFactory.getLogger(HelloResource.class);
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String sayHello() {
+        logger.info("Hello from Tomcat REST Service running in Docker!");
         return "Hello from Tomcat REST Service running in Docker!";
     }
 }
